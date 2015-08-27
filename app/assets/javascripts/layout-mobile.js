4a53d52f9d4abbe3e1fb68821d8950eedde27930
Davis Gao <naitnix@126.com>
2015-08-27 18:35:10 +0800

    debug

--- app/assets/javascripts/layout-mobile.js
+++ app/assets/javascripts/layout-mobile.js
@@ -24,7 +24,7 @@ $(function(){
 				}
 			}
 		});
-		//获取地理位置
+
 		wx.getLocation({
 		    type: 'wgs84',
 		    success: function (res) {
@@ -32,12 +32,12 @@ $(function(){
 		        var longitude = res.longitude; //经度
 		        var speed = res.speed; //速度，以米/每秒计
 		        var accuracy = res.accuracy; // 位置精度
-				// if($('#map').length > 0){
-				// 	var mp = new BMap.Map('map');
-				// 	mp.centerAndZoom(new BMap.Point(longitude, latitude), 11);
-				// }else{
-				// 	alert('地图不存在')
-				// }
+				if($('#map').length > 0){
+					var mp = new BMap.Map('map');
+					mp.centerAndZoom(new BMap.Point(longitude, latitude), 11);
+				}else{
+					alert('地图不存在')
+				}
 		    }
 		});
 	})	
