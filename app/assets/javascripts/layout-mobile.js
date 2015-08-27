4e29e43a668b40315057fa2d318e8d587f923349
Davis Gao <naitnix@126.com>
2015-08-27 17:56:47 +0800

    debug

--- app/assets/javascripts/layout-mobile.js
+++ app/assets/javascripts/layout-mobile.js
@@ -24,16 +24,5 @@ $(function(){
 				}
 			}
 		});
-		//获取地理位置
-		wx.getLocation({
-		    type: 'wgs84',
-		    success: function (res) {
-		        var latitude = res.latitude; //纬度
-		        var longitude = res.longitude; //经度
-		        var speed = res.speed; //速度，以米/每秒计
-		        var accuracy = res.accuracy; // 位置精度
-		        window.pos = [longitude,latitude]
-		    }
-		});
 	})	
 })
--- app/assets/javascripts/mobile/greenbelts-bundle.js
+++ app/assets/javascripts/mobile/greenbelts-bundle.js
@@ -1,10 +1,17 @@
 $(function(){
-	//初始化地图
 	function init(){
-		alert(window.pos[0]);
-		alert(window.pos[1]);  	
-		//var mp = new BMap.Map('map'); 
-		//mp.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  
+		//获取地理位置
+		wx.getLocation({
+		    type: 'wgs84',
+		    success: function (res) {
+		        var latitude = res.latitude; //纬度
+		        var longitude = res.longitude; //经度
+		        var speed = res.speed; //速度，以米/每秒计
+		        var accuracy = res.accuracy; // 位置精度
+		        var mp = new BMap.Map('map'); 
+		        mp.centerAndZoom(new BMap.Point(longitude, latitude), 11);  
+		    }
+		}); 
 	}	
 	init(); 	
 })
