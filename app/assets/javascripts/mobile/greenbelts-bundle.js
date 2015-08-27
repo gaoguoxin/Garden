8a8fbd6a6000290929101081472290ab51b949db
Davis Gao <naitnix@126.com>
2015-08-27 16:08:40 +0800

    debug

--- app/assets/javascripts/mobile/greenbelts-bundle.js
+++ app/assets/javascripts/mobile/greenbelts-bundle.js
@@ -3,8 +3,8 @@ $(function(){
 	function init(){
 		alert(window.position[0]);
 		alert(window.position[1]);  	
-		var mp = new BMap.Map('map'); 
-		mp.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  
+		//var mp = new BMap.Map('map'); 
+		//mp.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  
 	}	
 	init(); 	
 })
