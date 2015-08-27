492df4731ff3b6e274c08ff34073cb636d895b30
Davis Gao <naitnix@126.com>
2015-08-27 17:53:03 +0800

    debug

--- app/assets/javascripts/mobile/greenbelts-bundle.js
+++ app/assets/javascripts/mobile/greenbelts-bundle.js
@@ -1,8 +1,8 @@
 $(function(){
 	//初始化地图
 	function init(){
-		alert(window.position[0]);
-		alert(window.position[1]);  	
+		alert(window.pos[0]);
+		alert(window.pos[1]);  	
 		//var mp = new BMap.Map('map'); 
 		//mp.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  
 	}	
