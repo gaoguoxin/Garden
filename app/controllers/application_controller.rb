edb101cfa2aae3b90d3582d730d1fda967d8f6cc
Davis Gao <naitnix@126.com>
2015-08-28 16:01:11 +0800

    debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+  protect_from_forgery with: :null_session
   before_filter :authenticate_openid,:except => [:notify]
   before_filter :get_wx_config_info  
   layout :resolve_layout
