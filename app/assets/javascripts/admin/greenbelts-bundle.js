1fa4b60c3ca8d2d8d1844b9e5aba61a7eb29d35a
Davis Gao <naitnix@126.com>
2015-09-02 09:14:06 +0800

    debug

--- app/assets/javascripts/admin/greenbelts-bundle.js
+++ app/assets/javascripts/admin/greenbelts-bundle.js
@@ -1,15 +1,10 @@
 $(function(){
 	if($('#map').length > 0 ){
-    	var map = new BMap.Map("map", { mapType: BMAP_HYBRID_MAP });  
+    	var map = new BMap.Map("map", { mapType: BMAP_HYBRID_MAP,enableMapClick:false });  
     	map.centerAndZoom(new BMap.Point(116.30, 39.96), 14);
     	map.addControl(new BMap.MapTypeControl({mapTypes: [BMAP_NORMAL_MAP,BMAP_SATELLITE_MAP ]}));         
     	map.setCurrentCity("北京");
 
-		// var overlaycomplete = function(e){
-  //   	    drawingManager.close();
-  //   	    console.log(e.overlay)
-		// 	//console.log(BMapLib.GeoUtils.getPolygonArea(e.overlay))
-  //   	};
     	var styleOptions = {
     	    strokeColor:"red",
     	    fillColor:"red",
@@ -31,6 +26,13 @@ $(function(){
     	});  
     	drawingManager.addEventListener('polygoncomplete', function(e,overlay){
     		var paths = overlay.getPath();
+			var removeOlerlay = function(e,ee,overlay){
+				map.removeOverlay(overlay);
+			}
+			var markerMenu=new BMap.ContextMenu();
+			markerMenu.addItem(new BMap.MenuItem('删除',removeOlerlay.bind(overlay)));
+			overlay.addContextMenu(markerMenu);
+
     	});    	    	
 	}
 })
