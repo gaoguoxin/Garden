2c5b675945add4166b74166d1cf84e7049326c6e
yangxin <yangxin@oopsdata.com>
2015-09-06 15:11:23 +0800

    list

--- app/assets/javascripts/layout-pc.js
+++ app/assets/javascripts/layout-pc.js
@@ -1,3 +1,17 @@
 //= require global
 //= require drawing
 //= require geoUtils
+
+$(function(){
+	$(".nav-left").css("left",0-$(window).scrollLeft()+"px");
+	$(".nav-right").css("left",0-$(window).scrollLeft()+"px");
+	$(".welcome-head").css("left",0-$(window).scrollLeft()+"px");
+	$(".sidebar").css("left",0-$(window).scrollLeft()+"px");
+})
+
+$(window).scroll(function(event){
+	$(".nav-left").css("left",0-$(window).scrollLeft()+"px");
+	$(".nav-right").css("left",0-$(window).scrollLeft()+"px");
+	$(".welcome-head").css("left",0-$(window).scrollLeft()+"px");
+	$(".sidebar").css("left",0-$(window).scrollLeft()+"px");
+});
